76f6afc86b2f9c6284b713258e24eb1356a3002e
Casi McDaniels <casimcdaniels@gmail.com>
2018-05-10 15:42:59 -0400

    initialize hello world project with godep

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/*************************************************************
+
+
+	___________.__                      .__       .__
+	\_   _____/|  |   ____  __ _________|__| _____|  |__
+	 |    __)  |  |  /  _ \|  |  \_  __ \  |/  ___/  |  \
+	 |     \   |  |_(  <_> )  |  /|  | \/  |\___ \|   Y  \
+	 \___  /   |____/\____/|____/ |__|  |__/____  >___|  /
+		 \/                                     \/     \/
+
+						 M
+						dM
+						MMr
+					   4MMML
+					   MMMMM                 xf
+					  MMMMMM                MM-
+		Mh..          +MMMMMM             MMMM
+		 MMM           MMMMML           MMMMMh
+		  3MMMMx      'MMMMMMf      xnMMMMMMM
+		  '*MMMMM      MMMMMM      nMMMMMMPM
+			*MMMMMx    MMMMMM\     MMMMMMM=
+			   MMMMMM   3MMMM   dMMMMMM
+				MMMMMM  MMMMM   MMMMMM       MMMMM
+	=            *MMMMx  MMMM  dMMMMM     nnMMMMM*
+	  MMMn...     'MMMMr 'MM   MMMM    nMMMMMMM*M
+	   M4MMMMnn..   *MMM  MM  MMPM   dMMMMMMM""
+		 ^MMMMMMMMx   *ML MM  M*   MMMMMM**M
+			*PMMMMMMhn  *x > M   MMMM**""
+			   ""**MMMMhx/ h/  =*M
+						 3PM%....
+					  nPM     M*MMnx
+
+
+
+**********************************************************/
+
+func main () {
+	fmt.Println("Hello, world!")
+}
