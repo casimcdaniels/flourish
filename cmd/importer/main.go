eabfc681bb777727e1451910749fb2dffbfc352d
Casi McDaniels <casimcdaniels@gmail.com>
2018-05-10 22:35:52 -0400

    formatting of the fancy cannabis leaf

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -15,34 +15,34 @@ import (
 /*************************************************************
 
 
-	___________.__                      .__       .__
-	\_   _____/|  |   ____  __ _________|__| _____|  |__
-	 |    __)  |  |  /  _ \|  |  \_  __ \  |/  ___/  |  \
-	 |     \   |  |_(  <_> )  |  /|  | \/  |\___ \|   Y  \
-	 \___  /   |____/\____/|____/ |__|  |__/____  >___|  /
-		 \/                                     \/     \/
+___________.__                      .__       .__
+\_   _____/|  |   ____  __ _________|__| _____|  |__
+ |    __)  |  |  /  _ \|  |  \_  __ \  |/  ___/  |  \
+ |     \   |  |_(  <_> )  |  /|  | \/  |\___ \|   Y  \
+ \___  /   |____/\____/|____/ |__|  |__/____  >___|  /
+	 \/                                     \/     \/
 
-						 M
-						dM
-						MMr
-					   4MMML
-					   MMMMM                 xf
-					  MMMMMM                MM-
-		Mh..          +MMMMMM             MMMM
-		 MMM           MMMMML           MMMMMh
-		  3MMMMx      'MMMMMMf      xnMMMMMMM
-		  '*MMMMM      MMMMMM      nMMMMMMPM
-			*MMMMMx    MMMMMM\     MMMMMMM=
-			   MMMMMM   3MMMM   dMMMMMM
-				MMMMMM  MMMMM   MMMMMM       MMMMM
-	=            *MMMMx  MMMM  dMMMMM     nnMMMMM*
-	  MMMn...     'MMMMr 'MM   MMMM    nMMMMMMM*M
-	   M4MMMMnn..   *MMM  MM  MMPM   dMMMMMMM""
-		 ^MMMMMMMMx   *ML MM  M*   MMMMMM**M
-			*PMMMMMMhn  *x > M   MMMM**""
-			   ""**MMMMhx/ h/  =*M
-						 3PM%....
-					  nPM     M*MMnx
+					 M
+					dM
+					MMr
+				   4MMML
+				   MMMMM                 xf
+				  MMMMMM                MM-
+	Mh..          +MMMMMM             MMMM
+	 MMM           MMMMML           MMMMMh
+	  3MMMMx      'MMMMMMf      xnMMMMMMM
+	  '*MMMMM      MMMMMM      nMMMMMMPM
+		*MMMMMx    MMMMMM\     MMMMMMM=
+		   MMMMMM   3MMMM   dMMMMMM
+			MMMMMM  MMMMM   MMMMMM       MMMMM
+=            *MMMMx  MMMM  dMMMMM     nnMMMMM*
+  MMMn...     'MMMMr 'MM   MMMM    nMMMMMMM*M
+   M4MMMMnn..   *MMM  MM  MMPM   dMMMMMMM""
+	 ^MMMMMMMMx   *ML MM  M*   MMMMMM**M
+		*PMMMMMMhn  *x > M   MMMM**""
+		   ""**MMMMhx/ h/  =*M
+					 3PM%....
+				  nPM     M*MMnx
 
 
 
@@ -99,7 +99,7 @@ func main() {
 		}
 
 		processed++
-		
+
 		fmt.Printf("%d / %d imported \n", processed, num)
 	}
 }
