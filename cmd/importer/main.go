d12f24ded35baa5130f7a90a037789b306781d8e
Casi McDaniels <casimcdaniels@gmail.com>
2018-05-10 22:38:54 -0400

    different ascii leaf

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -21,29 +21,22 @@ ___________.__                      .__       .__
  |     \   |  |_(  <_> )  |  /|  | \/  |\___ \|   Y  \
  \___  /   |____/\____/|____/ |__|  |__/____  >___|  /
 	 \/                                     \/     \/
-
-					 M
-					dM
-					MMr
-				   4MMML
-				   MMMMM                 xf
-				  MMMMMM                MM-
-	Mh..          +MMMMMM             MMMM
-	 MMM           MMMMML           MMMMMh
-	  3MMMMx      'MMMMMMf      xnMMMMMMM
-	  '*MMMMM      MMMMMM      nMMMMMMPM
-		*MMMMMx    MMMMMM\     MMMMMMM=
-		   MMMMMM   3MMMM   dMMMMMM
-			MMMMMM  MMMMM   MMMMMM       MMMMM
-=            *MMMMx  MMMM  dMMMMM     nnMMMMM*
-  MMMn...     'MMMMr 'MM   MMMM    nMMMMMMM*M
-   M4MMMMnn..   *MMM  MM  MMPM   dMMMMMMM""
-	 ^MMMMMMMMx   *ML MM  M*   MMMMMM**M
-		*PMMMMMMhn  *x > M   MMMM**""
-		   ""**MMMMhx/ h/  =*M
-					 3PM%....
-				  nPM     M*MMnx
-
+                  |
+                 |.|
+                 |.|
+                |\./|
+                |\./|
+.               |\./|               .
+ \^.\          |\\.//|          /.^/
+  \--.|\       |\\.//|       /|.--/
+    \--.| \    |\\.//|    / |.--/
+     \---.|\    |\./|    /|.---/
+        \--.|\  |\./|  /|.--/
+           \ .\  |.|  /. /
+ _ -_^_^_^_-  \ \\ // /  -_^_^_^_- _
+   - -/_/_/- ^_^/| |\^_^ -\_\_\- -
+             /_ / | \ _\
+                  |
 
 
 /**********************************************************/
